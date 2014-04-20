2e7cbfe725d2c13b8281cb7ca942220cc7db4f8f
Evan Tarrh <evantarrh@gmail.com>
2014-04-20 02:54:03 -0400

    Adding site files

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+   e.preventDefault();
+   console.log("I like butts");
+   var randomColor = Math.floor(Math.random()*11);
+   if (randomColor = 1) {
+   	$('.icon').css('color', '#3cc')
+   }
+   if (randomColor = 2) {
+   	$('.icon').css('color', '#6D4B31')
+   }
+   if (randomColor = 3) {
+   	$('.icon').css('color', '#D6803C')
+   }
+   if (randomColor = 4) {
+   	$('.icon').css('color', '#D6819C')
+   }
+   if (randomColor = 5) {
+   	$('.icon').css('color', '#3CB2D6')
+   }
+   if (randomColor = 6) {
+   	$('.icon').css('color', '#51665A')
+   }
+   if (randomColor = 7) {
+   	$('.icon').css('color', '#604C66')
+   }
+   if (randomColor = 8) {
+   	$('.icon').css('color', '#B5AAB6')
+   }
+   if (randomColor = 9) {
+   	$('.icon').css('color', '#911')
+   }
+   if (randomColor = 10) {
+   	$('.icon').css('color', '#006')
+   }
+
+});

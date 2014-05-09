9b87ad9dcbd8fc7d1d82cc23bbc426cc338b151a
Evan Tarrh <evantarrh@gmail.com>
2014-05-09 09:40:22 -0400

    cleaned up JS & fixed bad color

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-   var colorArray = ['#3cc', '#6DFB31', '#D6803C', '#D6819C', '#3CB2D6', '#51665A', '#604C66', '#B5AAB6', '#911', '#006']
+   var colorArray = ['#3cc', '#6D4B31', '#D6803C', '#D6819C', '#3CB2D6', '#51665A', '#604C66', '#B5AAB6', '#911', '#006']
    var randomColor = 1 + Math.floor(Math.random() * 10);
    $('.icon').css('color', colorArray[randomColor]);
    

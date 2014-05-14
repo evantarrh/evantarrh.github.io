ca31211f18fada2bef661f567a021f3f922179b5
Evan Tarrh <evantarrh@gmail.com>
2014-05-14 01:02:10 -0400

    converted jQuery to vanilla.js, improved css

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+window.onload=function() {
+	console.log("Look ma, no jQuery!");
+	var colorArray = ['#3cc', '#504', '#D6803C', '#D6819C', '#3CB2D6', '#51665A', '#604C66', '#B5AAB6', '#911', '#006']
+	var randomColor = 1 + Math.floor(Math.random() * 10);
 
-   var colorArray = ['#3cc', '#6D4B31', '#D6803C', '#D6819C', '#3CB2D6', '#51665A', '#604C66', '#B5AAB6', '#911', '#006']
-   var randomColor = 1 + Math.floor(Math.random() * 10);
-   $('.icon').css('color', colorArray[randomColor]);
-   
+	var iconArray = [],
+	    list = document.querySelectorAll(".icon");
 
-});
+	for (var i = 0; i < list.length; i++ ) {
+	  iconArray[ i ] = list[ i ];
+	}
+
+	for (var i = 0; i < iconArray.length; i++ ) {
+	  iconArray[ i ].style.color = colorArray[ randomColor ];
+	}
+}

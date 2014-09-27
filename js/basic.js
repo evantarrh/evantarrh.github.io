5b09989ee13869b27af4910cfc7fbe2c3fe9f4f9
Evan Tarrh <evantarrh@gmail.com>
2014-09-26 20:08:04 -0400

    blinks on load, face hover turns cursor into a block

--- js/basic.js
+++ js/basic.js
@@ -1,7 +1,13 @@
 window.onload=function() {
-	console.log("Look ma, no jQuery!");
 	var colorArray = ['#3cc', '#504', '#D6803C', '#D681AB', '#3CB2D6', '#3CB2A1', '#604C66', '#B5AAB6', '#911', '#704807']
 	var randomColor = Math.floor(Math.random() * 9);
+
+	var faceBlocks = document.querySelectorAll('.face-row div');
+
+	for (var i = 0; i<faceBlocks.length; i++) {
+ 		faceBlocks[i].style.margin = "0.15rem 0.5rem";
+ 		faceBlocks[i].style.margin = "0rem";
+	}
 
 	var iconArray = [],
 	    list = document.querySelectorAll(".icon");
@@ -14,29 +20,47 @@ window.onload=function() {
 	  iconArray[ i ].style.color = colorArray[ randomColor ];
 	}
 
-	function wink(){
+	var glasses = document.querySelectorAll(".outer");
+	var eyes = document.querySelectorAll(".eyes");
+
+
+
+	function blink(){
 		setTimeout(function () {
-			for (var i = 0; i < iconArray.length; i++ ) {
-				iconArray[i].classList.add("hoveredicon");
+			for (var i = 0; i < glasses.length; i++ ) {
+				glasses[i].style.backgroundColor = "#f7e7e2";
+				if (i < 4) {
+					eyes[i].style.backgroundColor = "#000";
+				}
 			};
 		}, 600);
 		setTimeout(function () {
-			for (var i = 0; i < iconArray.length; i++ ) {
-				iconArray[i].classList.remove("hoveredicon");
+			for (var i = 0; i < glasses.length; i++ ) {
+				glasses[i].style.backgroundColor = "#000";
+				if (i < 4) {
+					eyes[i].style.backgroundColor = "#fff";
+				}
 			};
 		}, 700);
 		setTimeout(function () {
-			for (var i = 0; i < iconArray.length; i++ ) {
-				iconArray[i].classList.add("hoveredicon");
+			for (var i = 0; i < glasses.length; i++ ) {
+				glasses[i].style.backgroundColor = "#f7e7e2";
+				if (i < 4) {
+					eyes[i].style.backgroundColor = "#000";
+				}
 			};
 		}, 800);
 		setTimeout(function () {
-			for (var i = 0; i < iconArray.length; i++ ) {
-				iconArray[i].classList.remove("hoveredicon");
+			for (var i = 0; i < glasses.length; i++ ) {
+				glasses[i].style.backgroundColor = "#000";
+				if (i < 4) {
+					eyes[i].style.backgroundColor = "#fff";
+				}
 			};
 		}, 900);
 	}
 
-	wink();
+
+	blink();
 
 }

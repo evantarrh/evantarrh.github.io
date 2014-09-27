15b682e269b79135f9ca9d697ffc2f6a97655e15
Evan Tarrh <evantarrh@gmail.com>
2014-09-27 15:43:01 -0400

    bare-bones new design is ready for deployment

--- js/basic.js
+++ js/basic.js
@@ -1,23 +1,10 @@
 window.onload=function() {
-	var colorArray = ['#3cc', '#504', '#D6803C', '#D681AB', '#3CB2D6', '#3CB2A1', '#604C66', '#B5AAB6', '#911', '#704807']
-	var randomColor = Math.floor(Math.random() * 9);
 
 	var faceBlocks = document.querySelectorAll('.face-row div');
 
 	for (var i = 0; i<faceBlocks.length; i++) {
  		faceBlocks[i].style.margin = "0.15rem 0.5rem";
  		faceBlocks[i].style.margin = "0rem";
-	}
-
-	var iconArray = [],
-	    list = document.querySelectorAll(".icon");
-
-	for (var i = 0; i < list.length; i++ ) {
-	  iconArray[ i ] = list[ i ];
-	}
-
-	for (var i = 0; i < iconArray.length; i++ ) {
-	  iconArray[ i ].style.color = colorArray[ randomColor ];
 	}
 
 	var glasses = document.querySelectorAll(".outer");
